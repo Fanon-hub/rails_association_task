9fe9621220ac70608d03555899ddb729b8bbc87d
Fanon-hub <fanonwalker@gmail.com>
2025-10-23 09:09:57 +0300

    Fix all associations according to ER diagram

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
     has_many :orders 
     has_one :address, as: :addressable
+    belongs_to :address 
    
     # belongs_to :addressable, polymorphic: true
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,5 +1,6 @@
 class Shop < ApplicationRecord
     has_many :foods
     # belongs_to :addressable, polymorphic: true
-    has_one :address, as: :addressable
+    has_one :address, as: :addressable 
+    belongs_to :address
 end
